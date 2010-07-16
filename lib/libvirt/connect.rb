52fdf1a95e5847eadb92f4763256bda1af2c4425
henning mueller <henning@orgizm.net>
2010-07-16 20:05:27 +0200

    changed host information, more comments

--- lib/libvirt/connect.rb
+++ lib/libvirt/connect.rb
@@ -1,6 +1,7 @@
 # Extend Libvirt::Connect with a method which returns all available domains as
-# an array.
+# an array and several methods to access the host info more easily.
 class Libvirt::Connect
+	# Returns array of domain objects.
 	def domains
 		ret = Array.new
 
@@ -11,5 +12,47 @@ class Libvirt::Connect
 		end
 
 		ret
+	end
+
+	# Returns number of cores as string.
+	def cores
+		self.node_get_info.cores.to_s
+	end
+
+	# Returns number of CPUs as string.
+	def cpus
+		self.node_get_info.cpus.to_s
+	end
+
+	# Returns clock speed in GHz.
+	def ghz
+		"%.2f" % ( self.node_get_info.mhz / 1000.0 )
+	end
+
+	# Returns CPU architecture.
+	def arch
+		self.node_get_info.model
+	end
+
+	# Returns total memory in mebibytes as string.
+	def mem
+		( self.node_get_info.memory / 1024 ).to_s + 'M'
+	end
+
+	# Returns memory which is not allocated to virtual machines in mebibytes as
+	# string.
+	def memfree
+		memUsed = 0
+
+		self.domains.each do |domain|
+			memUsed += domain.info.memory
+		end
+
+		( ( self.node_get_info.memory - memUsed ) / 1024 ).to_s + 'M'
+	end
+
+	# Returns the libvirt version number.
+	def version
+		self.libversion.to_s
 	end
 end
--- lib/libvirt/domain.rb
+++ lib/libvirt/domain.rb
@@ -1,10 +1,12 @@
 # Extend Libvirt::Domain with different methods for human readable display of
 # the domain state.
 class Libvirt::Domain
+	# Default string representation of domain object is the domain's name.
 	def to_s
 		self.name
 	end
 
+	# Return state as string symbol.
 	def state
 		case( self.info.state )
 			when 1
@@ -16,18 +18,23 @@ class Libvirt::Domain
 		end
 	end
 
+	# Return current memory assignment in mebibytes as string.
 	def mem
 		( self.info.memory / 1024 ).to_s + "M"
 	end
 
+	# Return maximum memory assignment in mebibytes as string.
 	def mem_max
 		( self.info.max_mem / 1024 ).to_s + "M"
 	end
 
+	# Return the number of assigned virtual CPUs as string.
 	def cpu_num
 		self.info.nr_virt_cpu.to_s
 	end
 
+	# Return time the virtual domain's CPU was scheduled as string formatted
+	# d-hh:mm:ss.
 	def cpu_time
 		t = self.info.cpu_time / 10**9
 
